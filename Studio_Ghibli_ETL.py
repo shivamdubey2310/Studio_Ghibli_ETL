ccc2ba728980db4bee65cb84eac13a19d5eb0095
shivamdubey2310 <shivamdubey2310711@gmail.com>
2025-04-29 15:41:14 +0530

    A

--- Studio_Ghibli_ETL.py
+++ Studio_Ghibli_ETL.py
@@ -104,6 +104,49 @@ def extraction():
     logging.debug("Extraction for all endpoints was successful!!!")
 
 # -----------------------------------------------------------------------------------
+
+def jsonToDf(file_name):
+    """
+    Helper function to load a json file to dataframe
+    
+    Params:
+    -------
+        file_name(str) : Path of the file
+    """
+    
+    try:
+        with open(file_name, "r") as file_reader:
+            data_df = pd.read_json(file_reader)
+    
+    except FileNotFoundError as e:
+        logging.error("File {file_name} does not exist!!")
+    except Exception as e:
+        logging.error(f"An exception occured while opening {file_name} : {e}")
+    
+    return data_df
+
+
+def DfToJson(data_df, file_name):
+    """
+    Helper function to dump a dataframe to json
+    
+    Params:
+    -------
+        data_df(dataframe): a Dataframe to dump
+        file_name(str) : Path of the file to dump in
+    """
+
+    try:
+        with open(file_name, "w") as file_writer:
+            data_df = pd.read_json(file_writer)
+            data_df.to_json(file_writer)
+    
+    except FileNotFoundError as e:
+        logging.error("File {file_name} does not exist!!")
+    except Exception as e:
+        logging.error(f"An exception occured while opening {file_name} : {e}")
+
+
 def handling_missing_values(data_df, dict_null):
     """Function to handle missing values
     params
@@ -132,14 +175,8 @@ def detecting_missing_values(endpoint):
     
     # Opening file
     file_name = f"raw_json/{endpoint}_raw.json"
-    try:
-        with open(file_name, "r") as file_reader:
-            data_df = pd.read_json(file_reader)
-    except FileNotFoundError as e:
-        logging.error("File {file_name} does not exist!!")
-    except Exception as e:
-        logging.error(f"An exception occured while opening {file_name} : {e}")
-    
+    data_df = jsonToDf(file_name)
+
     # Checking missing values 
     null_value_count = data_df.isna().sum()
     null_value_count_dict = null_value_count.to_dict()
@@ -154,28 +191,120 @@ def detecting_missing_values(endpoint):
     else:
         logging.info(f"There are some null values in {endpoint}!!!")
         data_df = handling_missing_values(data_df, dict_null)
-        try:
-            with open(file_name, "w") as file_writer:
-                data_df.to_json(file_writer)
-        except Exception as e:
-            logging.error(f"An exception occured while writing {file_name} : {e}")
-
+        DfToJson(data_df, file_name)
     logging.info(f"Handling missing values successful for {endpoint}!!")
 
+
 def handling_duplicate_rows(endpoint):
-    pass
+    """Handling duplicate entries
+    params
+    ------
+        endpoint(str) : Name of the endpoint
+    """
+
+    logging.info(f"Handling duplicate values for : {endpoint}")
+    
+    file_name = f"raw_json/{endpoint}_raw.json"
+    data_df = jsonToDf(file_name)
+    
+    # Removing duplicate entries
+    data_df = data_df.drop_duplicates()
+
+    DfToJson(data_df, file_name)
+
+    logging.info(f"Handling duplicate values successful for {endpoint}!!")
+
+def creating_entity_ids(endpoint):
+    """Creating new ids (Overwriting older ids)
+    Params
+    -------
+        endpoint(str): name of the endpoint 
+    """
+
+    # Saving column_names for every endpoint data
+    file_name = f"raw_json/{endpoint}_raw.json"
+    data_df = jsonToDf(file_name)
+
+    # Overwriting ids
+    old_id_list = list(data_df["id"])
+    new_id = 1
+    mapping_dict = {}
+
+    for old_id in old_id_list:
+        if old_id not in mapping_dict.keys():
+            mapping_dict[old_id] = new_id
+            new_id += 1
+    
+    # Overwriting id column to new ids
+    data_df["id"] = data_df["id"].map(mapping_dict)
+
+    # Over-Writing raw json 
+    file_name = f"raw_json/{endpoint}_raw.json"
+    DfToJson(data_df, file_name)
+
+    logging.info(f"ID Overwriting for {endpoint} is successful!!")
+
+
+def mapping_endpoints(endpoint):
+    """
+    Establishing relationship between collections by creating a different entity_id and connecting with another collections
+    
+    Params
+    -------
+        endpoint(str): name of the endpoint 
+    """
+    
+    # Saving column_names for every endpoint data
+    file_name = f"raw_json/{endpoint}_raw.json"
+    data_df = jsonToDf(file_name)
+
+
 
 def data_cleaning(endpoint):
     """Function to clean data for endpoints"""
+    
+    # For missing values
     detecting_missing_values(endpoint)
-
+    
+    # For duplicate entries
     handling_duplicate_rows(endpoint)
+
+    logging.debug(f"Data cleaning for {endpoint} completed successfully!!")
 
 def transformation():
     """Main transformation function"""
 
     logging.info("In transformation phase")
     for endpoint in endpoint_list: 
-        data_cleaning(endpoint)
+        # data_cleaning(endpoint)
+
+        # For creating another id and relationship
+        creating_entity_ids(endpoint)
+
+        # For establishing relationship
+        # mapping_endpoints(endpoint)
 
 transformation()
+
+
+def testing():
+    list_1 = ["Shivam", "Nishita", "Shivam", "Hi"]
+    df = pd.DataFrame({
+        "name": list_1,
+        "class" : [1, 12, 23, 55]
+        }
+    )
+    name_list = list(df["name"])
+    dict1 = {}
+    id = 1
+    
+    for name in name_list:
+        if name not in dict1.keys():
+            dict1[name] = id
+            id += 1
+    
+    df["name"] = df["name"].map(dict1)
+
+    print(df)
+
+# testing()
